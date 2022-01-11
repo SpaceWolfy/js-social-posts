3318f9c7560954cf972698a0238213e778e569bb
Samuel Scozzari <s_scozzari@libero.it>
2022-01-11 15:53:41 +0100

    Milestone 2 completed - new posts printed on page

--- js/main.js
+++ js/main.js
@@ -5,14 +5,84 @@ const postsArray = [
   {
     idPost: 1,
     nomeAutore: "Samuel Scozzari",
+    fotoAutore: "https://data.whicdn.com/images/337883524/original.jpg",
+    data: new Date().toLocaleDateString(),
+    testo: "Ha aggiornato la sua foto profilo:",
+    immagine: "https://data.whicdn.com/images/337883524/original.jpg",
+    likes: 23,
+  },
+
+  {
+    idPost: 2,
+    nomeAutore: "user074859203",
     fotoAutore:
-      "https://media.wired.com/photos/5dd593a829b9c40008b179b3/191:100/w_2338,h_1224,c_limit/Cul-BabyYoda_mandalorian-thechild-1_af408bfd.jpg",
-    data: "01-11-2022",
-    testo:
-      "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+      "https://static.nanopress.it/nanopress/fotogallery/780X0/240679/super-mario.jpg",
+    data: new Date().toLocaleDateString(),
+    testo: "Ho appena creato il mio profilo, come faccio a seguire le persone?",
+    immagine: "",
+    likes: 2,
+  },
+
+  {
+    idPost: 3,
+    nomeAutore: "Boolean",
+    fotoAutore:
+      "https://coursereport-s3-production.global.ssl.fastly.net/uploads/school/logo/681/original/Pittogramma-RGB-1080-BLUE-circle.png",
+    data: new Date().toLocaleDateString(),
+    testo: "Iscriviti al corso!",
     immagine:
-      "https://i.picsum.photos/id/29/300/300.jpg?hmac=7V5Tl7xSnR3G7AHTEUIf7U3-gKgWbdX5cYzqh1W_6ZQ",
+      "https://stack-vod-learnn-source-9043g70p82tj.s3.eu-west-3.amazonaws.com/large_i_cover_boolean_01_a0978d807b.jpg",
+    likes: 217,
   },
 ];
 
 console.log(postsArray);
+
+/* Milestone 2 */
+//partendo dal layout di riferimento presente dall'HTML, ricreo il post con i dati che ho inserito nell'array
+const postsContainer = document.querySelector(".posts-list");
+
+for (let i = 0; i < postsArray.length; i++) {
+  const post = postsArray[i];
+  postsContainer.innerHTML += ` 
+<div class="post">
+  <div class="post__header">
+    <div class="post-meta">
+      <div class="post-meta__icon">
+        <img
+          class="profile-pic"
+          src="${post.fotoAutore}"
+        />
+      </div>
+      <div class="post-meta__data">
+        <div class="post-meta__author">${post.nomeAutore}</div>
+        <div class="post-meta__time">${post.data}</div>
+      </div>
+    </div>
+  </div>
+  <div class="post__text">
+    ${post.testo}
+  </div>
+  <div class="post__image">
+    <img src="${post.immagine}" alt="" />
+  </div>
+  <div class="post__footer">
+    <div class="likes js-likes">
+      <div class="likes__cta">
+        <a class="like-button js-like-button" href="#" data-postid="1">
+          <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+          <span class="like-button__label">Mi Piace</span>
+        </a>
+      </div>
+      <div class="likes__counter">
+        Piace a
+        <b id="like-counter-1" class="js-likes-counter">
+        ${post.likes}
+        </b>
+        persone
+      </div>
+    </div>
+  </div>
+</div>
+`;
+}
