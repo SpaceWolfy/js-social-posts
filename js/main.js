c6f1e12ebbd139a23f3f7b12733120ff698ea329
Samuel Scozzari <s_scozzari@libero.it>
2022-01-11 15:10:53 +0100

    Milestone 1 completed - objects array added

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* Milestone 1 */
+
+/* creo un array di oggetti che andrà a racchiudere la struttura dei vari post */
+const postsArray = [
+  {
+    idPost: 1,
+    nomeAutore: "Samuel Scozzari",
+    fotoAutore:
+      "https://media.wired.com/photos/5dd593a829b9c40008b179b3/191:100/w_2338,h_1224,c_limit/Cul-BabyYoda_mandalorian-thechild-1_af408bfd.jpg",
+    data: "01-11-2022",
+    testo:
+      "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    immagine:
+      "https://i.picsum.photos/id/29/300/300.jpg?hmac=7V5Tl7xSnR3G7AHTEUIf7U3-gKgWbdX5cYzqh1W_6ZQ",
+  },
+];
+
+console.log(postsArray);
